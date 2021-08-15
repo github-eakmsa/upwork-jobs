654807902446058e89cbcb0f822e8fd48f8517a0
Mehammedbrhan Abdelkadr <github-eakmsa@users.noreply.github.com>
2021-08-15 13:05:58 +0300

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,12 +1,12 @@
 <?php
 
 /* Google App Client Id */
-define('CLIENT_ID', '23241908110-gjvbajq81sc52hu8lodbk1g6j0ulgnvh.apps.googleusercontent.com');
+define('CLIENT_ID', 'xxxxxxxx');
 
 /* Google App Client Secret */
-define('CLIENT_SECRET', 'TEYgTCpyJlPYJxpMb9JcOmaV');
+define('CLIENT_SECRET', 'xxxxxxxxxx');
 
 /* Google App Redirect Url */
-define('CLIENT_REDIRECT_URL', 'http://localhost/upwork-jobs/google-cal-api/google-login.php');
+define('CLIENT_REDIRECT_URL', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
 
 ?>
